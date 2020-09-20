933bc7385bb0515063b3f5e282a65b920385bff4
tarundm <tarun4deshmukh@gmail.com>
2020-09-20 18:51:20 +0530

    Added XML based configuration chuck-config.xml

--- src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
@@ -2,9 +2,14 @@ package guru.springframework.joke.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
+
+    // @ImportResource annotation is part of XML based configuration
+    // chuck-config.xml is part of XML based configuration which is added inside resources
 
     public static void main(String[] args) {
         SpringApplication.run(JokeappApplication.class, args);
--- src/main/java/guru/springframework/joke/jokeapp/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -4,17 +4,22 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// ChuckNorrisQuotes is modified to demo Java based configuration
-@Configuration
+// ChuckNorrisQuotes is modified to demo Java based configuration.
+// @Configuration is part of java based configuration
+// @Configuration
 public class ChuckConfiguration {
 
     // Method name does not matter. it can be getChuckNorrisQuotes() or ChuckNorrisQuotes()
     // This is annotated with Bean so it will be available to entire project this is often
     // used for external jars and where team works on source which we don't have direct access to.
 
-    @Bean
+    // @Bean annotation is part of java based configuration
+    // @Bean
     public ChuckNorrisQuotes getChuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
+
+    // Java based configuration - once we comment these annotations, spring does not see this class
+    // as configuration class.
 
 }
